830494bf5bb9b0e0bfab3d266f2a934235b40140
hanner-V1 <hanner2372020@gmail.com>
2026-04-21 21:15:14 -0500

    Add files via upload

---
+++ Proyectos.py/primer_programa.py
@@ -0,0 +1,17 @@
+#Inicializacion variables y colocandoles un valor
+nombre="Hanner Leandro Rios Resyes"
+edad=15
+estado=True
+
+#Esta linea me imprime el contenido de una variable
+print("Bienvenido",nombre)
+print("Tu edad es:",edad)
+
+
+#Uso de la funcion input()
+nombre=input("¿Cual es tu nombre?")
+edad=input("¿Cual es tu edad?")
+
+#Mostrar el resultado capturado con input
+print("Bienvenido",nombre)
+print("Tu edad:",edad)
