bcf076db5b17bb32b9ebfb020772b19651ac84a7
hanner-V1 <hanner2372020@gmail.com>
2026-04-29 10:19:00 -0500

    Add files via upload

---
+++ Proyectos/Operaciones.py
@@ -0,0 +1,15 @@
+#Definimos variables para trabajar operaciones aritmeticas
+a=25
+b=50
+c=30
+d=9
+#Realizar las operaciones y guardamos el valor en su variable
+suma=a+b
+resta=c=d
+multiplicacion=a*b
+division=c/d
+#Mostramos el resultado de cada operacion
+print("La suma es:",suma)
+print("La resta es:",resta)
+print("La multiplicacion es:",multiplicacion)
+print("La division es:",division)
