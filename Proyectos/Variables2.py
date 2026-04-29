321d9fe2206d4c3c5da3d1f6bcd225130461fa4a
hanner-V1 <hanner2372020@gmail.com>
2026-04-29 10:06:17 -0500

    Add files via upload

---
+++ Proyectos/Variables2.py
@@ -0,0 +1,28 @@
+#Esto es un comentario
+'''Esto es un comentario de una variables'''
+#Definir variables
+nombre="Hanner Leandro Rios Reyes"
+edad=15
+estado=True
+nota=4,0
+#Mostrar resultado por consola
+print("Hola",nombre)
+print(edad)
+print(estado)
+print(nota)
+#Como saber el tipo de dato de cada variable
+print(type(nombre))
+print(type(edad))
+print(type(estado))
+print(type(nota))
+#Utilizar la funcion input()->permite recibir datos por medio del teclado
+nombre=input("Dime tu nombre:")
+edad=input("Dime tu edad:")
+estado=input("Dime tu estado:")
+nota=input("Dime tu nota:")
+#Mostrar resultado capturado en las variables
+print(nombre)
+print(edad)
+print(estado)
+print(nota)
+
