ccda256128df9bdcac369eb8e5e01e6e8081360b
hanner-V1 <hanner2372020@gmail.com>
2026-05-12 19:15:41 -0500

    Add files via upload

---
+++ Proyectos/Mi_Perfil_Digital.py
@@ -0,0 +1,20 @@
+#Uso de loa tipos de datos de python
+#1.Datos basicos(str,int,bool,float)
+nombre="Hanner Leandro Rios Reyes"
+edad=15
+estatura=1,66
+es_estudiante=True
+#2.Redes_sociales(type)
+Redes_sociales=("hanner-v1","leandro_r.ry")
+#3.playlist de canciones favoritas(list en un dict)
+playlist=[{"titulo":Jane,"artista":The_Long_Faces,"duracion":3.10},
+{"titulo":PARAISO(daña),"artista":Milo_j,"duracion":4.46},
+{"titulo":COQUETA,"artista":Fuerza_Regida_Grupo_Frontera,"duracion":4.00}]
+print("presentacion personal")
+print("Mi nombre es:",nombre)
+print("edad:",edad)
+print("Mi estatura:",estatura)
+print("¿estoy activo en el colegio?:",es_estudiante)
+print("Mi playlist favorita")
+print(f"{cancion['titulo']}-{cancion['artista']}-{cancion['duracion']}Min")
+print("---------")
